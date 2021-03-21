76ccbf841394c69c0c41270c7c18d1ed41cd30a6
Adityabhaumik <adi123bhaumik@gmail.com>
2021-03-21 11:29:01 +0530

    onsubmite bug fix in ask name screen

--- lib/utilities/askname_utility.dart
+++ lib/utilities/askname_utility.dart
@@ -6,62 +6,72 @@ import '../provider/name_provider.dart';
 AlertBoxAskName(BuildContext context) {
   TextEditingController controller = TextEditingController();
   var Nameprovider = Provider.of<NameNotifier>(context);
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: <Widget>[
-      Padding(
-        padding: const EdgeInsets.only(top:10.0),
-        child: Text(
-          'WELCOME 🙌',
-          style: Theme.of(context).textTheme.headline2,
-        ),
-      ),
-      SizedBox(
-        height: MediaQuery.of(context).size.height*0.18,
-      ),
-      Container(
-        child: TextFormField(
-          controller: controller,
-          cursorColor: Theme.of(context).secondaryHeaderColor,
-          style: TextStyle(
-            color: Theme.of(context).secondaryHeaderColor,
-          ),
-          decoration: InputDecoration(
-            fillColor: Theme.of(context).primaryColor,
-            filled: true,
-            labelText: "Enter Name",
-            labelStyle: TextStyle(
-                color: Theme.of(context).secondaryHeaderColor, fontSize: 15),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor, width: 2.0),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            focusColor: Theme.of(context).primaryColor,
-            hintText: 'Enter Name',
-            hintStyle: TextStyle(
-                color: Theme.of(context).secondaryHeaderColor, fontSize: 15),
-          ),
-        ),
-      ),
-
-      Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-          onPressed: () {
-            print(controller.text);
-            // controller.text="";
-            Nameprovider.saveName(controller.text);
-
-          },
+  void saveName(String value){
+    Nameprovider.saveName(value);
+  }
+  return SingleChildScrollView(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(top:10.0),
           child: Text(
-            'Submit',
-            style: TextStyle(color: Colors.white),
+            'WELCOME 🙌',
+            style: Theme.of(context).textTheme.headline2,
           ),
         ),
-      )
-    ],
+        SizedBox(
+          height: MediaQuery.of(context).size.height*0.18,
+        ),
+        Container(
+          child: TextFormField(
+            onFieldSubmitted: (value){
+              saveName(controller.text);
+            },
+
+            controller: controller,
+            cursorColor: Theme.of(context).secondaryHeaderColor,
+            style: TextStyle(
+              color: Theme.of(context).secondaryHeaderColor,
+            ),
+            decoration: InputDecoration(
+              fillColor: Theme.of(context).primaryColor,
+              filled: true,
+              labelText:"Enter Name",
+              labelStyle: TextStyle(
+                  color: Theme.of(context).secondaryHeaderColor, fontSize: 15),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                    color: Theme.of(context).secondaryHeaderColor, width: 2.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              focusColor: Theme.of(context).primaryColor,
+              hintText:'Enter Name',
+              hintStyle: TextStyle(
+                  color: Theme.of(context).secondaryHeaderColor, fontSize: 15),
+            ),
+
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ElevatedButton(
+            onPressed: () {
+              print(controller.text);
+              // controller.text="";
+              saveName(controller.text);
+
+            },
+            child: Text(
+              'Submit',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        )
+      ],
+    ),
   );
 }
 //           actions: <Widget>[
